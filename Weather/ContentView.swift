1280ef3b06b9f70e6df4c7206624b2f69d4b0462
Devon Arnone <87498263+DevonArnone@users.noreply.github.com>
2025-10-13 16:18:46 -0400

    10-day-forecast

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -14,24 +14,49 @@ struct Forecast: Identifiable {
     var temperature: String
 }
 
+struct WeekForecast: Identifiable {
+    var id = UUID()
+    var day: String
+    var icon: String
+    var temperature1: String
+    var temperature2: String
+}
+
 struct ContentView: View {
     var body: some View {
+        
         let hourlyData: [Forecast] = [
             Forecast(time: "Now", icon: "sun.max.fill", temperature: "55°"),
             Forecast(time: "1PM", icon: "sun.max.fill", temperature: "57°"),
             Forecast(time: "2PM", icon: "sun.max.fill", temperature: "58°"),
-            Forecast(time: "3PM", icon: "sun.max.fill", temperature: "57°"),
-            Forecast(time: "4PM", icon: "sun.max.fill", temperature: "56°")
+            Forecast(time: "3PM", icon: "cloud.sun.fill", temperature: "52°"),
+            Forecast(time: "4PM", icon: "sun.max.fill", temperature: "55°")
+        ]
+        
+        let weeklyData: [WeekForecast] = [
+            WeekForecast(day: "Today", icon: "cloud.fill", temperature1: "50°", temperature2: "52°"),
+            WeekForecast(day: "Mon", icon: "sun.max.fill", temperature1: "52°", temperature2: "50°"),
+            WeekForecast(day: "Tue", icon: "cloud.drizzle", temperature1: "54°", temperature2: "48°"),
+            WeekForecast(day: "Wed", icon: "cloud.rain", temperature1: "56°", temperature2: "48°"),
+            WeekForecast(day: "Thu", icon: "sun.max.fill", temperature1: "58°", temperature2: "48°"),
+            WeekForecast(day: "Fri", icon: "cloud.fill", temperature1: "50°", temperature2: "48°"),
+            WeekForecast(day: "Sat", icon: "snow", temperature1: "52°", temperature2: "48°"),
+            WeekForecast(day: "Sat", icon: "snow", temperature1: "52°", temperature2: "48°"),
+            WeekForecast(day: "Sat", icon: "sun.max.fill", temperature1: "52°", temperature2: "48°"),
+            WeekForecast(day: "Sat", icon: "cloud.fill", temperature1: "52°", temperature2: "48°"),
         ]
         
         VStack {
             Text("Chapel Hill")
             Text("55°")
             Text("Sunny")
-            Text("H:58° L:43°")
+            Text("H:58° L:50°")
             
             VStack(alignment: .leading) {
-                Text("Hourly Forecast")
+                HStack{
+                    Image(systemName: "clock")
+                    Text("Hourly Forecast")
+                }
                 
                 HStack {
                     ForEach (hourlyData) { item in
@@ -44,6 +69,24 @@ struct ContentView: View {
                 }
             }
             .background(Color.blue.opacity(0.6))
+            
+            VStack(alignment: .leading) {
+                HStack{
+                    Image(systemName: "calendar")
+                    Text("10-Day Forecast")
+                }
+                
+                VStack {
+                    ForEach (weeklyData) { item in
+                        WeekForecastView(
+                            day: item.day,
+                            icon: item.icon,
+                            temperature1: item.temperature1,
+                            temperature2: item.temperature2
+                        )
+                    }
+                }
+            }
         }
         .padding()
         .background(
@@ -62,6 +105,23 @@ struct HourlyForecastView: View {
             Text(time)
             Image(systemName: icon)
             Text(temperature)
+            
+        }
+    }
+}
+
+struct WeekForecastView: View {
+    var day: String
+    var icon: String
+    var temperature1: String
+    var temperature2: String
+    
+    var body: some View {
+        HStack {
+            Text(day)
+            Image(systemName: icon)
+            Text(temperature1)
+            Text(temperature2)
             
         }
     }
