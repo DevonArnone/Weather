249a6c2b3f88e83799322110faf18b4e3ae221eb
Devon Arnone <87498263+DevonArnone@users.noreply.github.com>
2025-10-13 15:50:34 -0400

    ForEach hourly data

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -7,29 +7,61 @@
 
 import SwiftUI
 
+struct Forecast: Identifiable {
+    var id = UUID()
+    var time: String
+    var icon: String
+    var temperature: String
+}
+
 struct ContentView: View {
     var body: some View {
+        let hourlyData: [Forecast] = [
+            Forecast(time: "Now", icon: "sun.max.fill", temperature: "55°"),
+            Forecast(time: "1PM", icon: "sun.max.fill", temperature: "57°"),
+            Forecast(time: "2PM", icon: "sun.max.fill", temperature: "58°"),
+            Forecast(time: "3PM", icon: "sun.max.fill", temperature: "57°"),
+            Forecast(time: "4PM", icon: "sun.max.fill", temperature: "56°")
+        ]
+        
         VStack {
             Text("Chapel Hill")
             Text("55°")
             Text("Sunny")
             Text("H:58° L:43°")
-            HStack {
+            
+            VStack(alignment: .leading) {
+                Text("Hourly Forecast")
                 
+                HStack {
+                    ForEach (hourlyData) { item in
+                        HourlyForecastView(
+                            time: item.time,
+                            icon: item.icon,
+                            temperature: item.temperature
+                        )
+                    }
+                }
             }
+            .background(Color.blue.opacity(0.6))
         }
         .padding()
+        .background(
+            LinearGradient(colors: [.blue, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+        )
     }
 }
 
-struct HourlyForecastView: Identifiable {
+struct HourlyForecastView: View {
     var time: String
     var icon: String
     var temperature: String
     
     var body: some View {
-        HStack {
+        VStack {
             Text(time)
+            Image(systemName: icon)
+            Text(temperature)
             
         }
     }
